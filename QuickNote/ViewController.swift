112e1a484fb14bcf614c1131e37d1b1d074f6d2c
busesahinbas <85345494+busesahinbas@users.noreply.github.com>
2023-12-08 23:12:10 +0300

    First commit

---
+++ QuickNote/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  QuickNote
+//
+//  Created by Buse Şahinbaş on 8.12.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
